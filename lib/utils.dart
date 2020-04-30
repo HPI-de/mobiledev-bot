a290b65b6add73a86fffc457f371b8031b692eb0
Marcel Garus <marcel.garus@gmail.com>
2020-04-30 18:34:27 +0200

    add random method

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:logger/logger.dart';
 
 final logger = Logger(
   filter: ProductionFilter(),
 );
+
+T random<T>(List<T> items) => items[Random().nextInt(items.length)];
